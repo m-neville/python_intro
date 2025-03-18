61b1734ce49afad078c8233f1181cc581f5953f3
Neville <ochiengnevil002@gmail.com>
2025-03-18 10:47:30 +0300

    python functions and conditions

--- number_functions.py
+++ number_functions.py
@@ -5,3 +5,34 @@ print(math.sqrt(x))
 print(math.pi)
 print(math.floor(78.88))
 print(math.ceil(78.15))
+
+radius = 54
+height = 78
+
+volume = 22/7 * radius * height
+volume = round(volume , 2)
+print(volume)
+
+sa = 2 * 22/7 * radius * (radius + height)
+sa = round(sa , 2)
+print(sa)
+
+result=10 + 20 / 5 * 6 - 3 * (5 + 2)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
